376a1bf1cdeb8c5c359eb17e8009995b149b52a3
Angel PC <pruebas@angel.com>
2024-05-24 23:19:48 -0500

    Cambio de monedas

--- return_change.cpp
+++ return_change.cpp
@@ -2,14 +2,14 @@
 #include <vector>
 using namespace std;
 
-bool return_change(int p, vector<int>& c, vector<int>& x) {
+bool return_change(int total, vector<int>& c, vector<int>& x) {
     int act = 0;
     int j = c.size() - 1;
-    while (act != p) {
-        while (c[j] > (p - act) && j > 0)
+    while (act != total) {
+        while (c[j] > (total - act) && j > 0)
         j--;
         if (j == 0) return false;
-        x[j] = (p - act) / c[j];
+        x[j] = (total - act) / c[j];
         act += c[j] * x[j];
     }
     return true;
@@ -18,8 +18,8 @@ int main() {
     //vector<int> monedas = { 1, 2, 5, 10, 20, 50, 100, 200}; // Todas con solucion
     vector<int> monedas = { 90, 100, 200 };
     vector<int> solucion(monedas.size(), 0);
-    int p = 180;
-    if (return_change(p, monedas, solucion)) {
+    int total = 180;
+    if (return_change(total, monedas, solucion)) {
         cout << "The change is: " << endl;
         for (int i = 0; i < solucion.size(); i++)
             if (solucion[i] != 0)
